afa15d0caf893a154f92ec02289f683ad67f44a8
Anton Henningsson <anton_9408@hotmail.com>
2023-03-27 13:00:33 +0200

    Lördag -> Måndag jobb, nästan klar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,97 @@
-import './App.css';
-import React, { useStatel, useEffect } from 'react';
+import "./App.css"
+import { TbArrowBarDown } from 'react-icons/tb';
+import ListItem from "./components/ListItem.js";
+import { useEffect, useState } from "react";
 
-function TodoList() {
-  const [ToDo, SetToDolist] = useState([]);
+const App = () => {
 
-useEffect(() => {
-    const storedTodos = localStorage.getItem('todos');
-    if (storedTodos) {
-      setTodos(JSON.parse(storedTodos));
-    } else {
-      setTodos(['Duscha', 'Träna', 'Äta', 'Sova', 'Spela']);
+    const [todo, setTodo] = useState("")
+    const [allTodos, setAllTodos] = useState([])
+
+    const addTodo = (e) => {
+        e.preventDefault()
+
+        const todoItem = {
+            id: new Date().getTime(),
+            text: todo,
+            isChecked: false
+        }
+
+        if(todo !== ""){
+            setAllTodos([...allTodos].concat(todoItem).reverse())
+            setTodo("")
+        }
+
+        console.log(allTodos)
     }
-  }, []);
 
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-}
-localStorage.setItem('todo', 'TodoList'); 
+    
 
+    const getAllTodos = () => {
+        let stored = JSON.parse(localStorage.getItem("todo"))
 
-const todo = localStorage.getItem('todo');
-
-console.log(todo); 
+        if(stored) {
+            setAllTodos(stored)
+        }
+    }
 
 
-localStorage.removeItem('todo');
+    const toggleChecked = (id) => {
+        let updatedTodos = [...allTodos].map(todo => {
+            if(todo.id === id){
+                todo.isChecked = !todo.isChecked
+            }
 
-function App() {
+            return todo
+        })
+
+        setAllTodos(updatedTodos)
+    }
+
+
+
+    const deleteTodo = (id) => {
+        const filteredTodo = allTodos.filter(todo => todo.id !== id)
+        setAllTodos(filteredTodo)
+    }
+
+
+    useEffect(() => {
+        getAllTodos()
+    }, [])
+
+
+    useEffect(() => {
+        localStorage.setItem("todo", JSON.stringify(allTodos))
+    }, [allTodos])
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <TodoList />
-      </header>
+        <div className="App_todo">
+            <form className="App_input_wrapper" onSubmit={addTodo} >
+                <input type={"text"} className="App_input" value={todo} onChange={(e) => setTodo(e.target.value)} />
+                <div className="App_input_button" onClick={addTodo} >
+                    <TbArrowBarDown size={24} />
+                </div>
+            </form>
+
+            <div className="App_todo_list">
+                {
+                    allTodos.map(todo => (
+                        <ListItem key={todo.id} deleteTodo={() => deleteTodo(todo.id)} text={todo.text} isChecked={todo.isChecked} toggleChecked={() => toggleChecked(todo.id)} />
+                    ))
+                }
+
+                {
+                    allTodos.length === 0 && (
+                        <p className="empty">There are no Todo's</p>
+                    )
+                }
+            </div>
+        </div>
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
